8acc053b4943cb6471e3d362c21829f2f0e04592
Your Name <you@example.com>
2022-04-21 16:43:06 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -112,11 +112,11 @@ Pair * nextTreeMap(TreeMap * tree) {
         next=tree->current->right;
         tree->current=next;
         //return next->pair;
-    }
+    }/*
     if( tree->current->left!=NULL){
         next=tree->current->left;
         tree->current=next;
         //return next->pair;
-    }    
+    }    */
     return next->pair;
 }
