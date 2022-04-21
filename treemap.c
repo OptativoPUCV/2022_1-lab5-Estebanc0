86eabe6e4371aefbcc8218b4120967e5194b551b
Your Name <you@example.com>
2022-04-21 12:02:54 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -47,8 +47,14 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
-    //TreeNode*aux=tree->root;
-    tree->root->left=searchTreeMap(tree,key);
+    TreeNode*aux=tree->root;
+    TreeNode*new=createTreeNode(key,value);
+    while(aux!=NULL){
+        if(searchTreeMap(tree,key)==NULL){
+            tree->current=new;
+        }else return NULL;
+    }
+    
 
 }
 
@@ -77,6 +83,7 @@ void eraseTreeMap(TreeMap * tree, void* key){
 Pair * searchTreeMap(TreeMap * tree, void* key) {
     TreeNode* aux= tree->root;
     while(aux!=NULL){
+        tree->current=aux;//guarda el ultimo nodo valido
         if(tree->lower_than (key,aux->pair->key)==1){
             aux=aux->left;
         }else if(tree->lower_than (aux->pair->key,key)==1){
