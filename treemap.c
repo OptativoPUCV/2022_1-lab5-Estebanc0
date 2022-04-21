b20f44f5b9745e7b1eee23692898c299e9d8856d
Your Name <you@example.com>
2022-04-21 14:19:03 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -103,7 +103,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
 Pair * firstTreeMap(TreeMap * tree) {
     TreeNode* first=minimum(tree->root);
     tree->current=first;
-    return first;
+    return tree->current;
 }
 
 Pair * nextTreeMap(TreeMap * tree) {
