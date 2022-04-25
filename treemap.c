ddda5f7b0af932d9929b83f4dc69e3226d2f2e96
Your Name <you@example.com>
2022-04-24 22:43:32 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -48,7 +48,7 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
    TreeNode* aux=tree->root;  
-   TreeNode* new=createTreeNode(key,value);
+   
    while(aux!=NULL){
        if(tree->lower_than(key,aux->pair->key)==1){
            aux=aux->left;
